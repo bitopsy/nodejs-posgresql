a2cb427018a06561168d634670ef310e217234ab
b i t O Ψ <32198261+bitopsy@users.noreply.github.com>
2022-07-16 12:59:24 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.json({message: 'alive'});
 });
 
+var Flights = require('../Node_Skiplagged_API/index.js');
+
+var data = {
+          FROM: 'PAR',
+          TO: 'AJA',
+          DEPARTURE_DATE: '2022-07-17',
+          SKIP_HIDDEN_CITY: false,
+          RESULTS: 1
+
+};
+
+var CurrentFlight = new Flights(data);
+
+CurrentFlight.getFlightData(function(error, body) {
+          body = JSON.stringify(body, undefined, 4);
+          console.log(body);
+});
+
+
 module.exports = router;
