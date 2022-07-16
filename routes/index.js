2813803367dfbe00c69e73bec02c01dd04b0d4c2
b i t O Ψ <32198261+bitopsy@users.noreply.github.com>
2022-07-16 13:28:23 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.json({message: 're-alive'});
 });
-/*
-var Flights = require('../index.js');
-*/
-var data = {
-          FROM: 'PAR',
-          TO: 'AJA',
-          DEPARTURE_DATE: '2022-07-17',
-          SKIP_HIDDEN_CITY: false,
-          RESULTS: 1
 
-};
-
-/*
-var CurrentFlight = new Flights(data);
-
-CurrentFlight.getFlightData(function(error, body) {
-          body = JSON.stringify(body, undefined, 4);
-          console.log(body);
-});
-*/
 
 module.exports = router;
